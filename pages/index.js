727415454b75543527f78c2a196865349e3a4fcf
hieulv <hieu.lv@zinza.com.vn>
2022-06-23 10:54:44 +0700

    getProfile

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,16 @@ import Cookie from "js-cookie";
 import Router from "next/router";
 import { useEffect } from "react";
 import useSWR, { mutate } from "swr";
+import { getProfile } from "../libs/auth-api";
 
 export default function HomePage() {
   const accessToken = Cookie.get("access_token");
   const refreshToken = Cookie.get("refresh_token");
 
-  const storage = async (key) => {
-    const value = localStorage.getItem(key);
-    return !!value ? JSON.parse(value) : undefined;
-  };
-
-  const { data } = useSWR("auth", storage);
+  const { data } = useSWR("user", getProfile);
+  console.log({data});
 
   const handleLogoutClick = async () => {
-    window.localStorage.removeItem("auth");
     Cookie.remove("access_token");
     Cookie.remove("refresh_token");
     mutate("auth", null);
@@ -30,6 +26,7 @@ export default function HomePage() {
     <div>
       <h1>Home page</h1>
       <button onClick={handleLogoutClick}>Logout</button>
+      <button onClick={getProfile}>Get profile</button>
     </div>
   );
 }
